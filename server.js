ecec3bdf0c0aa2f44104f4e55919ae2047b43a37
Ian <ian@macbook-pro.lan>
2020-05-13 15:51:56 -0500

    using express to serve the html files rather than handlebars

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 var express = require("express");
-var exphbs = require("express-handlebars");
+// var exphbs = require("express-handlebars");
+
 
 var db = require("./models");
 
@@ -24,19 +25,20 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
+
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(__dirname));
 
 // Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
+// app.engine(
+//   "handlebars",
+//   exphbs({
+//     defaultLayout: "main"
+//   })
+// );
+// app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/apiRoutes")(app);
